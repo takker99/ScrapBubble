07205045ca547c16d2cdcdf7ef8e24e396412eb5
takker99 <37929109+takker99@users.noreply.github.com>
2021-12-12 11:07:14 +0900

    :recycle: Refactor loading system and cache system

--- hooks/useCards.ts
+++ hooks/useCards.ts
@@ -1,7 +1,10 @@
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext"/>
+/// <reference lib="dom"/>
 import { useCallback, useMemo, useState } from "../deps/preact.tsx";
-import { toLc } from "../utils.ts";
-import { Page } from "../deps/scrapbox.ts";
-import type { Scrapbox } from "https://pax.deno.dev/scrapbox-jp/types@0.0.5";
+import { exposeState, toLc } from "../utils.ts";
+import type { Page, Scrapbox } from "../deps/scrapbox.ts";
+import { getPage } from "../fetch.ts";
 declare const scrapbox: Scrapbox;
 
 export interface Cache {
@@ -30,10 +33,11 @@ export interface UseCardsInit {
 export function useCards(
   { expired = 60, whiteList: _whiteList = [] }: UseCardsInit,
 ) {
-  const [caches, setCaches] = useState(new Map<string, Cache>());
-  // 表示するデータのcache idのリストと表示位置とのペアのリスト
-  const [selectedList, setSelectedList] = useState<
-    { ids: string[]; position: Position }[]
+  const [
+    /** 表示するデータのcache id のリストと表示位置とのペアのリスト */ selectedList,
+    setSelectedList,
+  ] = useState<
+    { projects: string[]; title: string; position: Position }[]
   >([]);
 
   // このリストにあるproject以外は表示しない
@@ -42,146 +46,96 @@ export function useCards(
     [_whiteList],
   );
 
-  /** 特定のprojectのpageの情報をcacheする */
-  const _cache = useCallback(async (project: string, titleLc: string) => {
-    const id = toId(project, titleLc);
-    const now = new Date().getTime() / 1000; // cacheの検証開始時刻
-    let _expired = true;
-    setCaches((oldCaches) => {
-      const { checked = 0, lines = [], linked = [] } = oldCaches.get(id) ?? {};
-      _expired = checked + expired < now;
-      oldCaches.set(id, {
-        project,
-        titleLc,
-        loading: _expired,
-        lines,
-        linked,
-        checked: now,
-      });
-      return new Map(oldCaches);
-    });
-    if (!_expired) return;
-
-    // cacheが寿命切れのときは再生成する
-    let cache: Cache | undefined = undefined;
-    try {
-      cache = await fetchPage(project, titleLc);
-    } catch (e) {
-      console.error(e);
-    } finally {
-      setCaches((oldCaches) => {
-        const { lines = [], linked = [], checked = now } = oldCaches.get(id) ??
-          {};
-        oldCaches.set(id, {
-          project,
-          titleLc,
-          loading: false,
-          // cacheの取得に失敗していたら更新しない
-          lines: cache?.lines ?? lines,
-          linked: cache?.linked ?? linked,
-          checked,
-        });
-        return new Map(oldCaches);
-      });
-    }
-  }, [expired]);
-
-  // whiteList中のprojectに存在する同名のページを全てcacheする
-  const cache = useCallback(async (project: string, titleLc: string) => {
-    // whiteListにないprojectの場合は何もしない
-    if (!whiteList.includes(project)) return;
-    // whiteListにあるprojectについてもcacheしておく
-    for (const _project of whiteList) {
-      await _cache(_project, titleLc);
-    }
-  }, [_cache, whiteList]);
-  // depth階層目にカードを表示する
+  /** depth階層目にカードを表示する */
   const show = useCallback(
-    (depth: number, project: string, titleLc: string, position: Position) => {
+    (depth: number, project: string, title: string, position: Position) => {
       // whiteListにないprojectの場合は何もしない
       if (!whiteList.includes(project)) return;
 
-      setSelectedList((list) => {
-        const ids = whiteList.map((_project) => toId(_project, titleLc));
-        return [...list.slice(0, depth), { ids, position }];
-      });
+      setSelectedList((
+        list,
+      ) => [...list.slice(0, depth), {
+        // 指定されたprojectを最優先する
+        projects: [project, ...whiteList.filter((proj) => proj !== project)],
+        title,
+        position,
+      }]);
     },
     [whiteList],
   );
-  // depth階層以降のカードを消す
+
+  /** depth階層以降のカードを消す */
   const hide = useCallback(
-    (depth: number) => setSelectedList((list) => [...list.slice(0, depth)]),
+    (depth: number) => setSelectedList((list) => list.slice(0, depth)),
     [],
   );
 
-  const cards = useMemo(
+  /** 表示するbubblesの情報を取得する函数
+   *
+   * 取得中の場合は`loading`が`true`になる
+   */
+  const getBubbles = useMemo(
     () => {
       // 以前の階層のtext bubbleで使ったページはcard bubbleに使わない
-      const showedPages = [{
-        project: scrapbox.Project.name,
-        titleLc: toLc(scrapbox.Page.title ?? ""),
-      }];
-      return selectedList.flatMap(({ ids, position }) => {
-        const cacheList = ids.flatMap((id) =>
-          caches.has(id) ? [caches.get(id)!] : []
-        );
-        // text bubbleで表示するページを決める
-        const { project, titleLc, lines } = cacheList
-          .find((cache) => cache.lines.length > 0) ?? cacheList[0];
-        const linked = cacheList.flatMap(
-          ({ project, linked }) => linked.map((page) => ({ ...page, project })),
-        );
-        const card = {
-          project,
-          titleLc,
-          position,
-          lines: titleLc === showedPages[0].titleLc ? [] : lines, // editorで開いているページは表示しない
-          linked: linked.flatMap(({ project: _project, title, ...page }) =>
-            !showedPages.some((page) =>
-                page.project === _project && page.titleLc === toLc(title)
+      const showedPages = [toLc(scrapbox.Page.title ?? "")];
+
+      // ページデータを取得してbubblesを作る
+      const tasks = selectedList.map(({ projects, title, position }) =>
+        exposeState((async () => {
+          const pages = await Promise.all(projects.map((
+            project,
+          ) => getPage(project, title, { expired })));
+
+          // text bubbleで表示するページを決める
+          const titleLc = toLc(title);
+          const previewdPages = showedPages.includes(titleLc)
+            ? []
+            : pages.flatMap(({ persistent, title, lines }, i) =>
+              persistent
+                ? [{
+                  project: projects[i],
+                  title,
+                  lines,
+                }]
+                : []
+            );
+          showedPages.push(titleLc);
+
+          return {
+            pages: previewdPages,
+            position,
+            cards: pages.flatMap((page) =>
+              getBackLinks(page).filter(({ title }) =>
+                !showedPages.includes(toLc(title))
               )
-              ? [{ project: _project, title, ...page }]
-              : []
-          ),
-          loading: cacheList.every(({ loading }) => loading),
-        };
-        showedPages.push({ project, titleLc });
-        // linesもlinkedも空のときは消す
-        return card.lines.length > 0 || card.linked.length > 0 ? [card] : [];
-      });
+            ),
+          };
+        })())
+      );
+
+      return () =>
+        tasks.map((task) => {
+          const promiseState = task();
+          switch (promiseState.state) {
+            case "pending":
+              return { loading: true } as const;
+            case "fulfilled":
+              return { loading: false, ...promiseState.result } as const;
+            case "rejected":
+              throw promiseState.result;
+          }
+        });
     },
-    [caches, selectedList],
+    [selectedList, expired],
   );
 
-  return { cards, cache, show, hide };
+  return { getBubbles, show, hide };
 }
 
-function toId(project: string, titleLc: string) {
-  return `/${project}/${titleLc}`;
-}
-async function fetchPage(
-  project: string,
-  titleLc: string,
-): Promise<Cache | undefined> {
-  const res = await fetch(`/api/pages/${project}/${titleLc}`);
-  const checked = new Date().getTime() / 1000;
-  // 存在しないページの時
-  if (!res.ok) return;
-  const { lines, links, relatedPages: { links1hop } }: Page = await res.json();
-
-  // 逆リンクを取得する
+/** 逆リンクを取得する */
+function getBackLinks({ links, relatedPages: { links1hop } }: Page) {
   const linksLc = links.map((link) => toLc(link));
-  const pages = links1hop.flatMap(({
-    title,
-    descriptions,
-    image,
-  }) => !linksLc.includes(toLc(title)) ? [{ title, descriptions, image }] : []);
-  return {
-    project,
-    titleLc,
-    checked,
-    loading: false,
-    lines: lines.slice(1), // titleを除く
-    linked: pages,
-  };
+  return links1hop.flatMap((card) =>
+    linksLc.includes(toLc(card.title)) ? [] : [card]
+  );
 }
