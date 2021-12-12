f0cb7151e9111a6bde19454bdb89e5f855fb4695
takker99 <37929109+takker99@users.noreply.github.com>
2021-12-12 11:06:13 +0900

    :recycle: Use promises synchronously

--- utils.ts
+++ utils.ts
@@ -4,3 +4,23 @@ export const encodeTitle = (title: string) =>
     /[/?#\{}^|<>]/g,
     (char) => encodeURIComponent(char),
   );
+
+export type PromiseState<T, E> = {
+  state: "pending";
+  result: undefined;
+} | {
+  state: "fulfilled";
+  result: T;
+} | {
+  state: "rejected";
+  result: E;
+};
+
+// deno-lint-ignore no-explicit-any
+export function exposeState<T, E = any>(promise: Promise<T>) {
+  let state: PromiseState<T, E> = { state: "pending", result: undefined };
+  promise
+    .then((result) => state = { state: "fulfilled", result })
+    .catch((result) => state = { state: "rejected", result });
+  return () => state;
+}
