e900004f57a010363f3de5993c435d563ea8fbc0
takker99 <37929109+takker99@users.noreply.github.com>
2021-12-15 20:46:01 +0900

    :bug: Render correct scrapbox links

---
+++ parseLink.test.ts
@@ -0,0 +1,64 @@
+import { parseLink } from "./parseLink.ts";
+import { assertEquals } from "./deps/testing.ts";
+
+Deno.test("external link", () => {
+  assertEquals(
+    parseLink({
+      pathType: "root",
+      href: "/project/title#61b5253a1280f00000003d6b5e",
+    }),
+    {
+      project: "project",
+      title: "title",
+      hash: "61b5253a1280f00000003d6b5e",
+    },
+  );
+  assertEquals(
+    parseLink({ pathType: "root", href: "/project/title#invalidId" }),
+    {
+      project: "project",
+      title: "title#invalidId",
+    },
+  );
+  assertEquals(parseLink({ pathType: "root", href: "/project/title" }), {
+    project: "project",
+    title: "title",
+  });
+  assertEquals(parseLink({ pathType: "root", href: "/project/" }), {
+    project: "project",
+  });
+  assertEquals(parseLink({ pathType: "root", href: "/project" }), {
+    project: "project",
+  });
+  assertEquals(
+    parseLink({ pathType: "root", href: "/project/#title-with-#" }),
+    {
+      project: "project",
+      title: "#title-with-#",
+    },
+  );
+});
+Deno.test("internal link", () => {
+  assertEquals(
+    parseLink({
+      pathType: "relative",
+      href: "title#61b5253a1280f00000003d6b5e",
+    }),
+    {
+      title: "title",
+      hash: "61b5253a1280f00000003d6b5e",
+    },
+  );
+  assertEquals(
+    parseLink({ pathType: "relative", href: "title#invalidId" }),
+    {
+      title: "title#invalidId",
+    },
+  );
+  assertEquals(parseLink({ pathType: "relative", href: "title" }), {
+    title: "title",
+  });
+  assertEquals(parseLink({ pathType: "relative", href: "title/with#/" }), {
+    title: "title/with#/",
+  });
+});
---
+++ parseLink.ts
@@ -0,0 +1,27 @@
+export type ScrapboxLink = {
+  href: string;
+  pathType: "root" | "relative";
+};
+export function parseLink(
+  link: ScrapboxLink,
+) {
+  if (link.pathType === "root") {
+    const [, project = "", title = ""] = link.href.match(
+      /\/([\w\-]+)(?:\/?|\/(.*))$/,
+    ) ?? ["", "", ""];
+    if (project === "") {
+      throw SyntaxError(`Failed to get a project name from "${link.href}"`);
+    }
+    const [, hash] = title?.match?.(/#([a-f\d]{24,32})$/) ?? ["", ""];
+    return title === ""
+      ? { project }
+      : hash === ""
+      ? { project, title }
+      : { project, title: title.slice(0, -1 - hash.length), hash };
+  } else {
+    const [, hash] = link.href.match(/#([a-f\d]{24,32})$/) ?? ["", ""];
+    return hash === ""
+      ? { title: link.href }
+      : { title: link.href.slice(0, -1 - hash.length), hash };
+  }
+}
