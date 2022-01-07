b3519745a05c64fb327580afe811b85132e459b9
takker99 <37929109+takker99@users.noreply.github.com>
2022-01-07 09:58:15 +0900

    :bug: Fix type error of katex

--- deps/useKaTeX.ts
+++ deps/useKaTeX.ts
@@ -1,1 +1,1 @@
-export { useKaTeX } from "https://pax.deno.dev/takker99/scrapbox-katex-previewer/useKaTeX.ts";
+export { useKaTeX } from "https://raw.githubusercontent.com/takker99/scrapbox-katex-previewer/0.1.4/useKaTeX.ts";
