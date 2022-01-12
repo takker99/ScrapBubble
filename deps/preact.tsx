bac981346f0077ebb34e1420e21567cba82b39f0
takker99 <37929109+takker99@users.noreply.github.com>
2022-01-13 08:54:07 +0900

    :arrow_up:

--- deps/preact.tsx
+++ deps/preact.tsx
@@ -9,6 +9,6 @@ export {
   h,
   render,
   toChildArray,
-} from "https://esm.sh/preact@10.5.14";
-export type { ComponentChildren } from "https://esm.sh/preact@10.5.14";
-export * from "https://esm.sh/preact@10.5.14/hooks";
+} from "https://esm.sh/preact@10.6.4";
+export type { ComponentChildren } from "https://esm.sh/preact@10.6.4";
+export * from "https://esm.sh/preact@10.6.4/hooks";
