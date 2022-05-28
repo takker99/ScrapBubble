58274f8614f88e4c0872f33a099a9383a294c312
GitHub <noreply@github.com>
2022-05-28 00:41:35 +0000

    :arrow_up: update deno dependencies

--- deps/useKaTeX.ts
+++ deps/useKaTeX.ts
@@ -1,1 +1,1 @@
-export { useKaTeX } from "https://raw.githubusercontent.com/takker99/scrapbox-katex-previewer/0.3.1/useKaTeX.ts";
+export { useKaTeX } from "https://raw.githubusercontent.com/takker99/scrapbox-katex-previewer/0.3.2/useKaTeX.ts";
