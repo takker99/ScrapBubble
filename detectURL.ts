632a177b3d78b1ab217dc3803c5591fdaf381f35
takker99 <37929109+takker99@users.noreply.github.com>
2022-10-29 13:39:53 +0900

    :recycle: UserCSS挿入機能を切り出した

---
+++ detectURL.ts
@@ -0,0 +1,22 @@
+/** URL文字列をURLに変換する*/
+export const detectURL = (
+  text: URL | string,
+  base?: URL | string,
+): URL | string => {
+  if (text instanceof URL) return text;
+  try {
+    return new URL(text);
+  } catch (e: unknown) {
+    if (!(e instanceof TypeError)) throw e;
+    if (!base) return text;
+    // 相対パスへの変換を試みる
+    // ./や../や/で始まらない文字列は、相対パス扱いしない
+    if (!/^\.\/|\.\.\/|\//.test(text)) return text;
+    try {
+      return new URL(text, base);
+    } catch (e: unknown) {
+      if (!(e instanceof TypeError)) throw e;
+      return text;
+    }
+  }
+};
