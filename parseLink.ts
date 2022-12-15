d7dd8cc32f2931529d534098d2a08fb8c3bdb621
takker99 <37929109+takker99@users.noreply.github.com>
2022-12-16 08:39:51 +0900

    :recycle: parseLinkにJSDOcを付けた

--- parseLink.ts
+++ parseLink.ts
@@ -1,10 +1,20 @@
-export type ScrapboxLink = {
+export interface ScrapboxLink {
   href: string;
   pathType: "root" | "relative";
-};
+}
+
+/** Scrapbox記法のリンクから、project, title, hashを取り出す
+ *
+ * @param link 構文解析したScrapbox記法のリンク
+ * @return 抽出したproject, title, hash
+ */
 export const parseLink = (
   link: ScrapboxLink,
-) => {
+): { project: string; title?: string; hash?: string } | {
+  project?: string;
+  title: string;
+  hash?: string;
+} => {
   if (link.pathType === "root") {
     const [, project = "", title = ""] = link.href.match(
       /\/([\w\-]+)(?:\/?|\/(.*))$/,
