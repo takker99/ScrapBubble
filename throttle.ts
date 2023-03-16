c5b0ee1f5657c745bf45644eaa382021d4d08d48
takker99 <37929109+takker99@users.noreply.github.com>
2023-03-16 17:39:19 +0900

    :+1: Fetch a latest pushed job

--- throttle.ts
+++ throttle.ts
@@ -2,6 +2,8 @@ export type Fn<T> = () => Promise<T>;
 
 /** 同時に一定数だけPromiseを走らせる函数
  *
+ * 最後に追加したjobから実行する
+ *
  * @param max 同時に動かすPromiseの最大数
  * @return jobを動かして結果を返す函数
  */
@@ -15,7 +17,7 @@ export const makeThrottle = <T>(max: number): (job: Fn<T>) => Promise<T> => {
 
   const runNext = (prev: Promise<unknown>) => {
     pendings.delete(prev);
-    const task = queue.shift();
+    const task = queue.pop();
     if (!task) return;
     const promise = task[0]()
       .then((result) => task[1](result))
