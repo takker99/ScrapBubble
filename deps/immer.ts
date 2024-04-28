dbec16f9074811dc83e9e36be15aa59b9ff3f396
GitHub <noreply@github.com>
2024-04-28 00:38:39 +0000

    :arrow_up: update deno dependencies

--- deps/immer.ts
+++ deps/immer.ts
@@ -1,4 +1,4 @@
-import { enableMapSet } from "https://esm.sh/immer@10.0.4/";
+import { enableMapSet } from "https://esm.sh/immer@10.1.1/";
 enableMapSet();
 
-export * from "https://esm.sh/immer@10.0.4/";
+export * from "https://esm.sh/immer@10.1.1/";
