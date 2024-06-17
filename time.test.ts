039cf3df8c67c4232f9a467589ea02d7a0fd26c0
takker99 <37929109+takker99@users.noreply.github.com>
2024-06-18 08:39:21 +0900

    refactor: Remove an unused module

--- time.test.ts
+++
@@ -1,6 +0,0 @@
-import { getUnixTime } from "./time.ts";
-import { assertEquals } from "./deps/testing.ts";
-
-Deno.test("getUnixTime", () => {
-  assertEquals(getUnixTime(new Date(1671721200000)), 1671721200);
-});
